2b63e3b3e324da73f0012c8b77d22977ae8c8598
muhamad ridwan <muhamad.ridwan@mitrais.com>
2016-07-25 08:49:53 +0700

    packages

---
+++ hello-angular/web/bundles/js/laminarin.js
@@ -0,0 +1,23 @@
+﻿
+$(document).ready(function () {
+    $(".laminarin-form").on("change", ".uploadfile", function (e) {
+        readURL(this, $(this).attr("elm-view"));
+    });
+});
+
+function readURL(input,target) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $(target).attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+function getDefaultImage(elm)
+{
+    $(elm).attr('src', "/bundles/images/no_image_available.jpg");
+}
