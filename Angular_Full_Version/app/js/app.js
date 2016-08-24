3410492c6a77aa760d3f2b0e4dd25226c91a0571
muhamad ridwan <muhamad.ridwan@mitrais.com>
2016-08-24 16:48:40 +0700

    inspinia theme

---
+++ Angular_Full_Version/app/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ *
+ */
+ var app = angular.module('inspinia', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',                 // Ui Bootstrap
+        'pascalprecht.translate',       // Angular Translate
+        'ngIdle',                       // Idle timer
+        'ngSanitize'                    // ngSanitize
+    ]);
+//(function () {
+    
+//})();
+
+// Other libraries are loaded dynamically in the config.js file using the library ocLazyLoad
--- angular-app/app/js/app.js
+++ angular-app/app/js/app.js
@@ -1,5 +1,13 @@
 'use strict';
 
+angular.module('inspinia', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',                 // Ui Bootstrap
+        'pascalprecht.translate',       // Angular Translate
+        'ngIdle',                       // Idle timer
+        'ngSanitize'                    // ngSanitize
+    ]);
 var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
     .config(function($routeProvider, $locationProvider) {
         $routeProvider.when('/',
